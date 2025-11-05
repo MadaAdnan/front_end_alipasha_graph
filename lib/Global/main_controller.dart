41a5d7325909c87cbbdc4562c66e8fda13963bb1
sham <albasha.sq@gmail.com>
2025-11-05 10:40:10 +0300

    fix some design

--- lib/Global/main_controller.dart
+++ lib/Global/main_controller.dart
@@ -89,7 +89,7 @@ class MainController extends GetxController {
   RxList<SliderModel> sliders = RxList<SliderModel>([]);
   RxList<CountryModel> countries = RxList<CountryModel>([]);
   RxList<PricingModel> pricing = RxList([]);
-  String versionAPK = "3.3.0";
+  String versionAPK = "3.3.1";
   RxInt communityNotification = RxInt(0);
   RxBool startApp = RxBool(true); //for fill data from storage
   Rx<SettingModel> settings =
